2e0e87215eed48a44c7da3a1c0618996f2505bd7
Geraldo Fernandes da Silva Junior <geraldojrcg@A307-14L.imd.local>
2019-08-22 14:21:03 -0300

    first commit

---
+++ src/br/ufrn/imd/material/repositorios/UsuarioRepositorio.java
@@ -0,0 +1,29 @@
+package br.ufrn.imd.material.repositorios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.ufrn.imd.material.dominio.Usuario;
+
+public class UsuarioRepositorio {
+	
+	public static List<Usuario> usuarios;
+	
+	public static Usuario getUsuario(String login, String senha) {
+		if(usuarios == null) {
+			usuarios = new ArrayList<Usuario>();
+		}
+		Usuario usuario = new Usuario("geraldo", "geraldo");
+		usuario.setNome("Geraldo");
+		usuarios.add(usuario);
+		
+		for(Usuario u : usuarios) {
+			if(u.getLogin().equals(login) && u.getSenha().equals(senha)) {
+				return u;
+			}
+		}
+		
+		return null;
+	}
+
+}
